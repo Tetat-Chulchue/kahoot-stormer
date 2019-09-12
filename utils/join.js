b49af371b236baa0450dac7af02f07bad1459d71
Tetat <tetat.chulchue@gmail.com>
2019-09-12 20:54:01 +0700

    V0.1

--- utils/join.js
+++ utils/join.js
@@ -7,6 +7,7 @@ module.exports =  async function botJoin(name, botNum, gamePin) {
     console.log(botName + " is Joining kahoot...");
     bot.join(gamePin, botName).then(() =>{
         console.log(botName+ ' Has Been Joined!')
+        bot.leave();
         return true;
     });
 }
